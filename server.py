2fb968abed772c83bc6bb8f654a8cd94e0a77e22
Mayur Rokade <mrokade@linnkedin.com>
2015-11-06 20:53:36 +0530

    posting job + script work

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.restless import APIManager
+import os
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/schedulord.db'
+app.config['UPLOAD_FOLDER'] = os.path.abspath('.') + '/scripts'
 db = SQLAlchemy(app)
 
 
@@ -14,16 +17,46 @@ class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text)
     interval = db.Column(db.Integer)
-    func = db.Column(db.LargeBinary)
+    script_path = db.Column(db.Text)
     next_runtime = db.Column(db.DateTime)
 
 
+# App routes
+@app.route('/jobs')
+def index():
+    all_jobs = Job.query.all()
+    all_jobs = { x.name: {'id': x.id, 'name': x.name, 'interval': x.interval} for x in all_jobs}
+    return jsonify(jobs=all_jobs)
 
-manager = APIManager(app, flask_sqlalchemy_db=db)
 
-manager.create_api(Job, methods=['GET', 'POST', 'DELETE', 'PATCH'], url_prefix="/api", collection_name="jobs", allow_patch_many=True)
+@app.route('/jobs/create', methods=['POST'])
+def create():
+    file = request.files['file']
+    json_data = request.form
+    filename = secure_filename(file.filename)
+    script_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(script_path)
+    job = Job(name=json_data['name'],
+            interval=json_data['interval'],
+            next_runtime=None,
+            script_path=script_path
+            )
+    db.session.add(job)
+    db.session.commit()
+    return "{0} has been uploaded".format(filename)
+
+
+@app.route('/jobs/<id>', methods=['GET'])
+def read(id):
+    return "read method"
+
+
+@app.route('/jobs/<id>', methods=['DELETE'])
+def delete(id):
+    return "deletemethod"
+
 
 if __name__ == "__main__":
     db.drop_all()
     db.create_all()
-    app.run()
+    app.run(debug=True)
