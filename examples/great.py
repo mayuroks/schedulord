549515dea93beb2a6889ea1fd449f8d77da9f8f1
Mayur Rokade <mrokade@linnkedin.com>
2015-11-07 00:14:09 +0530

    added some example files

---
+++ examples/great.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+def main():
+    print datetime.now()
---
+++ examples/hello.py
@@ -0,0 +1,13 @@
+def hello():
+    print "Hello"
+
+def wow():
+    print "wow"
+
+def main():
+    '''
+    The scheduler will look for function 'main'
+    to execute
+    '''
+    hello()
+    wow()
